86b4b515132177ddcb2e112b4eb2385620503bba
afaw1 <arifwafa010@gmail.com>
2026-06-25 22:39:50 +0300

    ✅ Tuples  [python] — approach1

---
+++ solutions/python/easy/python-tuples/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Problem     Tuples 
+# Difficulty  Easy
+# Subdomain   Basic Data Types
+# Platform    HackerRank
+# Language    python
+# Status      Accepted
+# Submitted   2026-06-25, 10:39 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    t = tuple(integer_list)
+    print hash(t)
